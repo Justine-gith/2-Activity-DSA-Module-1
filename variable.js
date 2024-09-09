21895f08d8ad9c74d6d32d3c497f1c33d5e3c288
unknown <ganijustine8@gmail.com>
2024-09-09 11:20:33 +0800

    initial commit

---
+++ variable.js
@@ -0,0 +1,17 @@
+//Create a variable named name and store your name.
+let name = "Justine Gani.";
+
+//Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer".
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+//Create a variable named gender and store your gender.
+let gender = "Male";
+
+//Create a variable named twitterHandle and store your Twitter handle.
+let twitterHandle = "@Gani_Justine";
+
+//Log all the variables to the console using the code runner.
+console.log("What is your Name? " , name);
+console.log("What do you wanna become in your life?" , whatDoYouWannaBecomeInYourLife);
+console.log("Are you Male or Female? " ,gender);
+console.log("My twitter: " , twitterHandle);
